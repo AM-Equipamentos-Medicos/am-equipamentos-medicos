77000f99920a2f7150b0e547448ec5e16a738a28
AM-Equipamentos-Medicos <aequipamentosmedicos@gmail.com>
2025-06-25 16:35:49 -0400

    Update produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -1,23 +1,23 @@
 const produtos = [
-    {
-        nome: "Mamógrafo AIDIA",
-        categoria: "mamografia",
-        imagem: "img/produto_1.jpg",
-        especificacoes: "Detector FOCUS Csl, alta resolução, conforto e precisão para mamografia digital.",
-        whatsapp: "Mamógrafo AIDIA"
-    },
-    {
-        nome: "Agulha de Biópsia UNIGUN",
-        categoria: "biopsia",
-        imagem: "img/produto_2.jpg",
-        especificacoes: "Ponta trifacetada, ecogênica, compatível com pistola Alpha Core.",
-        whatsapp: "Agulha de Biópsia UNIGUN"
-    },
-    {
-        nome: "Raio-X Móvel Pégaso",
-        categoria: "radiologia",
-        imagem: "img/produto_3.jpg",
-        especificacoes: "125KV/500mA, gerador de alta frequência, ideal para mobilidade e precisão.",
-        whatsapp: "Raio-X Móvel Pégaso"
-    }
+  {
+    nome: "Mamógrafo AIDIA",
+    categoria: "mamografia",
+    imagem: "img/produto_1.jpg",
+    especificacoes: "Detector FOCUS Csl, alta resolução.",
+    whatsapp: "Mamógrafo AIDIA"
+  },
+  {
+    nome: "Agulha UNIGUN",
+    categoria: "biopsia",
+    imagem: "img/produto_2.jpg",
+    especificacoes: "Ponta ecogênica, compatível com Alpha Core.",
+    whatsapp: "Agulha UNIGUN"
+  },
+  {
+    nome: "Raio-X Móvel Pégaso",
+    categoria: "radiologia",
+    imagem: "img/produto_3.jpg",
+    especificacoes: "Alta frequência, 125KV/500mA.",
+    whatsapp: "Raio-X Pégaso"
+  }
 ];
